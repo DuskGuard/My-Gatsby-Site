770243bdda11bef719f424d93b290bc66473680f
DuskGuard <trentapt@gmail.com>
2019-04-01 23:36:01 -0600

    Revised Some Input Fields and Links, More To Come

--- src/components/Main.js
+++ src/components/Main.js
@@ -51,24 +51,10 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
+          <h2 style={{textTransform:"capitalize"}}>Email Me At <i style={{textTransform:"lowercase"}}>trentapt@gmail.com</i></h2>
+          <br/>
+          <h2 style={{textTransform:"capitalize"}}>Or Contact Me On My Social Media Below!</h2>
+          <hr/>
           <ul className="icons">
             <li><a href="https://www.linkedin.com/in/trent-apt/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
             <li><a href="https://www.instagram.com/prettygood_guy/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
